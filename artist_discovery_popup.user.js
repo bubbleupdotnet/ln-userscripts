7871fc1755e7f4c747a703880ed2892339695b3a
bubbleupdotnet <admin@bubbleup.net>
2022-08-24 12:08:23 -0500

    added styling dependent on url

--- artist_discovery_popup.user.js
+++ artist_discovery_popup.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Ticketmaster/Live Nation EDP Discovery Popup
 // @namespace    http://bubbleup.net/
-// @version      0.1
+// @version      0.2
 // @description  Shows an alert popup on TM EDP to give you the discovery ID
 // @author       daniel@bubbleup.net
 // @match        https://www.ticketmaster.com/*/artist/*
 // @match        https://www.livenation.com/artist/*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ticketmaster.com
-// @grant        GM_addStyle
+// @grant        none
 // @downloadURL  https://github.com/bubbleupdotnet/ln-userscripts/raw/main/artist_discovery_popup.user.js
 // ==/UserScript==
 
@@ -51,35 +51,76 @@
         return t
     }
 
-    var details = getDiscoveryInfo();
-    var message = formatObjectToString(details);
+    function addStyling() {
 
-    var textarea = document.createElement('textarea');
-    textarea.innerHTML = message;
+        var style = document.createElement('style');
+        style.setAttribute('id', 'popup-styling');
 
-    var copyButton = document.createElement('button');
-    copyButton.className = 'copy-button';
-    copyButton.innerHTML = 'Copy Content';
-    copyButton.onclick = function() {
-        navigator.clipboard.writeText(textarea.value).then(() => {
-            alert("Copied to clipboard!");
-        });
-    };
-
-    var popupEndText = document.createElement('p');
-    popupEndText.innerHTML = 'Tip: You can disable this popup in the TamperMonkey extension.';
-
-    document.body.appendChild(popup);
-    popup.appendChild(closeButton);
-    popup.appendChild(textarea);
-    popup.appendChild(copyButton);
-    popup.appendChild(popupEndText);
-})();
-
-GM_addStyle ( `
-    #popup-container {
+        // Styles popup container based on url
+        if (window.location.toString().includes("livenation")) {
+            style.textContent = `
+     #popup-container {
         position: absolute;
-        top: 8%;
+        top: 18%;
+        left: 62%;
+        margin: 0;
+        padding: 9px;
+        background: #E21836;
+        box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;;
+        display: block;
+        width: 26%;
+        height: 28%;
+    }
+
+    .close-button {
+        float: right;
+        background: transparent;
+        border: none;
+        color: white;
+        font-size: 1.5em;
+        cursor: pointer;
+    }
+
+    textarea {
+        background: #E21836;
+        color: white;
+        font-size: 1.5em;
+        text-align: center;
+        width: 100%;
+        height: 60%;
+        resize: none;
+        outline: none;
+        border: none;
+        display: block;
+        margin-top: 1.5em;
+    }
+
+    .copy-button {
+        background: rgb(255, 255, 255);
+        border: solid black;
+        border-radius: .25em;
+        color: black;
+        font-size: 1em;
+        margin: 0 auto;
+        cursor: pointer;
+        display: block;
+        margin-bottom: .75em;
+    }
+
+    .copy-button:hover {
+       background: #ccc;
+       cursor: pointer;
+    }
+
+    p {
+        font-size: .6em;
+        color: white;
+    }`
+        } else {
+            style.textContent = `
+     #popup-container {
+        position: absolute;
+        top: 10%;
         left: 38.25%;
         margin: 0;
         padding: 9px;
@@ -105,11 +146,11 @@ GM_addStyle ( `
         font-size: 1.25em;
         text-align: center;
         width: 100%;
-        height: 55%;
+        height: 60%;
         resize: none;
         outline: none;
         border: none;
-        margin: 0 auto;
+        display: block;
     }
 
     .copy-button {
@@ -118,9 +159,9 @@ GM_addStyle ( `
         border-radius: .25em;
         color: black;
         font-size: 1em;
-        text-align: center;
-        margin: 0 9em;
+        margin: 0 auto;
         cursor: pointer;
+        display: block;
     }
 
     .copy-button:hover {
@@ -129,7 +170,38 @@ GM_addStyle ( `
     }
 
     p {
-        font-size: .75em;
+        font-size: .5em;
         color: white;
+    }`
     }
-` );
+
+        document.head.appendChild(style);
+
+    }
+
+    var details = getDiscoveryInfo();
+    var message = formatObjectToString(details);
+
+    var textarea = document.createElement('textarea');
+    textarea.innerHTML = message;
+
+    var copyButton = document.createElement('button');
+    copyButton.className = 'copy-button';
+    copyButton.innerHTML = 'Copy Content';
+    copyButton.onclick = function() {
+        navigator.clipboard.writeText(textarea.value).then(() => {
+            alert("Copied to clipboard!");
+        });
+    };
+
+    var popupEndText = document.createElement('p');
+    popupEndText.innerHTML = 'Tip: You can disable this popup in the TamperMonkey extension.';
+
+    addStyling();
+
+    document.body.appendChild(popup);
+    popup.appendChild(closeButton);
+    popup.appendChild(textarea);
+    popup.appendChild(copyButton);
+    popup.appendChild(popupEndText);
+})();
